778bd7ce2afbf231bc92bbfb8b0a033e16b4a987
LK Loh <lkloh2410@gmail.com>
2017-10-23 09:51:12 -0700

    Fixed users

--- debate/urls.py
+++ debate/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^user_list/$', views.user_list, name='user_list'),
+    url(r'^users/$', views.users, name='users'),
+    url(r'^posts/$', views.posts, name='posts'),
 ]
--- debate/views.py
+++ debate/views.py
@@ -4,14 +4,17 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from debate.models import User
+from debate.models import User, Post, Comment
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world")
 
-def user_list(request):
-	user_list = [user for user in User.objects.all()]
-	#return HttpResponse('User')
-	return render(request, 'debate/user_list.html', {'user_list': user_list})
+def users(request):
+	users = [user for user in User.objects.all()]
+	return render(request, 'debate/users.html', {'users': users})
+
+def posts(request):
+	posts = [post for post in Post.objects.all()]
+	return render(request, 'debate/posts.html', {'posts': posts})
