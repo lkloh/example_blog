2aba510fa51f7ebcc076f652843d3af3e576c287
LK Loh <lkloh2410@gmail.com>
2017-10-23 09:21:45 -0700

    Render

--- debate/views.py
+++ debate/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from debate.models import User
 
 # Create your views here.
 
@@ -11,5 +12,6 @@ def index(request):
     return HttpResponse("Hello world")
 
 def user_list(request):
-	return HttpResponse('User')
-	# render(request, 'debate/user_list.html')
+	user_list = [user for user in User.objects.all()]
+	#return HttpResponse('User')
+	return render(request, 'debate/user_list.html', {'user_list': user_list})
