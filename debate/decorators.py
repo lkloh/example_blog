7e6f7d09b1507c087436eaf30091c7e8cf13155d
LK Loh <lkloh2410@gmail.com>
2017-10-25 09:33:33 -0700

    this sucks

--- debate/decorators.py
+++ debate/decorators.py
@@ -5,6 +5,6 @@ def get_num_queries(func):
         db.reset_queries()
         func(*args, **kwargs)
         num_queries = sum(len(conn.queries) for conn in db.connections.all())
-        return num_queries
+        print "Num queries: %d" % num_queries
     return wrapper
 
--- debate/tests.py
+++ debate/tests.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import unittest
+
 from django.test import TestCase
 
+
+from debate.decorators import get_num_queries
+from debate.models import User, Post, Comment
+
+
 # Create your tests here.
+
+class UserModelTests(TestCase):
+
+	@get_num_queries
+	def test_user_name_empty_string(self):
+		empty_name_user = User("")
+		self.assertEquals(empty_name_user.name, "")
+
